d0045bb86c7396c4c77fce4f19c029e670aa49c0
natorcutt <orcuttna@msu.edu>
2016-06-30 09:50:20 -0500

    Responsive nav

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-$( ".cross" ).hide();
-$( ".menu" ).hide();
-$( ".hamburger" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".hamburger" ).hide();
-$( ".cross" ).show();
-});
-});
+//$( ".cross" ).hide();
+//$( ".menu" ).hide();
+//$( ".hamburger" ).click(function() {
+   // $( ".menu" ).slideToggle( "slow", function() {
+      //  $( ".hamburger" ).hide();
+      //  $( ".cross" ).show();
+//});
+//});
 
-$( ".cross" ).click(function() {
-$( ".menu" ).slideToggle( "slow", function() {
-$( ".cross" ).hide();
-$( ".hamburger" ).show();
-});
-});
+//$( ".cross" ).click(function() {
+ //   $( ".menu" ).slideToggle( "slow", function() {
+ //           $( ".cross" ).hide();
+          //      $( ".hamburger" ).show();
+//});
+//});
+
+
+//Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+}
